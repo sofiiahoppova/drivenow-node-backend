fb09e1da638a3982f0fcf2eac0ac9f084cb360e1
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-09 11:03:58 +0200

    feat(app): integrate migglewars and route handling

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import userRoute from "./routes/userRoute.js";
 
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 app.use("/users", userRoute);
+
+app.use("/*splat", notFoundHandler);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
