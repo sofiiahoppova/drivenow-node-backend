2e5fdf2657fbbf44a1406197fd15bd4d7214cf14
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-09 11:05:51 +0200

    refactor(routes): wrap user controllers with ctrlWrapper

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
+
 import {
   createUser,
   deleteUser,
   getUsers,
   updateUser,
 } from "../controllers/userController.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.post("/", ctrlWrapper(createUser));
+
+router.get("/", ctrlWrapper(getUsers));
+
+router.put("/:id", ctrlWrapper(updateUser));
+
+router.delete("/:id", ctrlWrapper(deleteUser));
 
 export default router;
