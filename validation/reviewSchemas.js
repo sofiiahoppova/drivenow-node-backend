ff35a4f5b698e22e7e2951b05e3afddeee3df280
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-13 15:38:53 +0200

    feat(validation): add review create and update schemas

---
+++ validation/reviewSchemas.js
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+export const createReviewValidationSchema = Joi.object({
+  description: Joi.string().required().messages({
+    "string.base": "Description must be a string",
+    "string.empty": "Description is required",
+    "any.required": "Please provide the description",
+  }),
+  rating: Joi.number().min(0).max(5).required().messages({
+    "number.base": "Rating must be a number",
+    "any.required": "Please add rating",
+  }),
+  carId: Joi.number().integer().required().messages({
+    "number.base": "Car Id must be a number",
+    "number.integer": "Car Id must be a valid integer",
+    "any.required": "Please add car id",
+  }),
+  userId: Joi.number().integer().required().messages({
+    "number.base": "User Id must be a number",
+    "number.integer": "User Id must be a valid integer",
+    "any.required": "Please add user id",
+  }),
+});
+
+export const updateReviewValidationSchema = createReviewValidationSchema.fork(
+  ["description", "rating", "carId", "userId"],
+  (schema) => schema.optional()
+);
