9233c93dabcc8ab81418994c2c64325ac1292361
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-16 18:28:43 +0200

    feat(utils): add functions for generating access and refresh tokens

---
+++ utils/generateToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = (id) => {
+  return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+export const generateRefreshToken = (id) => {
+  return jwt.sign({ id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+};
