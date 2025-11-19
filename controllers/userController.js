738950d9c77cfc5ebd5442145957d375d5a15d90
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-19 17:36:31 +0200

    refactor(controllers): add hashing passwords, some checks and auth use

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
-import createHttpError from "http-errors";
 import { PrismaClient } from "@prisma/client";
+import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
-export const getUsers = async (req, res) => {
+export const getAllUsers = async (req, res) => {
   const users = await prisma.user.findMany();
 
   return res.status(200).json({
@@ -14,34 +15,24 @@ export const getUsers = async (req, res) => {
 };
 
 export const getUser = async (req, res) => {
-  const { id } = req.params;
-  const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(req.user.id) },
+    include: { bookings: true },
+  });
+
+  if (!user) {
+    throw createHttpError(404, "User not found");
+  }
 
   return res.status(200).json({
     status: 200,
-    message: `Successfully found user with id ${id}`,
+    message: `Successfully found user with id ${req.user.id}`,
     data: user,
   });
 };
 
-export const createUser = async (req, res) => {
-  if (await prisma.user.findUnique({ where: { email: req.body.email } })) {
-    throw createHttpError(409, `${req.body.email} user already exists`);
-  }
-
-  const newUser = await prisma.user.create({
-    data: { ...req.body },
-  });
-
-  return res.status(201).json({
-    status: 200,
-    message: "Successfully created user",
-    data: newUser,
-  });
-};
-
 export const updateUser = async (req, res) => {
-  const { id } = req.params;
+  const id = req.user.id;
   const user = await prisma.user.findUnique({
     where: { id: parseInt(id) },
   });
@@ -50,28 +41,37 @@ export const updateUser = async (req, res) => {
     throw createHttpError(404, "User not found");
   }
 
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      phoneNumber: req.body.phoneNumber,
-      NOT: { id: parseInt(id) },
-    },
-  });
-  if (existingUser) {
-    throw createHttpError(422, "User with this phone number already exists");
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+    throw createHttpError(401, "Wrong password. Unauthorised");
+  }
+
+  if (req.body.phoneNumber) {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        phoneNumber: req.body.phoneNumber,
+        NOT: { id: parseInt(id) },
+      },
+    });
+    if (existingUser) {
+      throw createHttpError(409, "User with this phone number already exists");
+    }
   }
 
   if (req.body.email) {
-    const existingUser = await prisma.user.findUnique({
+    const existingUser = await prisma.user.findFirst({
       where: { email: req.body.email },
+      NOT: { id: parseInt(id) },
     });
 
-    if (existingUser && existingUser.id !== user.id) {
+    if (existingUser) {
       throw createHttpError(409, `${req.body.email} is already taken`);
     }
   }
 
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
   const updatedUser = await prisma.user.update({
-    data: { ...req.body },
+    data: { ...req.body, password: hashedPassword },
     where: { id: parseInt(id) },
   });
 
@@ -84,7 +84,7 @@ export const updateUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
   const user = await prisma.user.findUnique({
-    where: { id: parseInt(req.params.id) },
+    where: { id: parseInt(req.user.id) },
   });
 
   if (!user) {
@@ -93,7 +93,5 @@ export const deleteUser = async (req, res) => {
 
   await prisma.user.delete({ where: { id: parseInt(req.params.id) } });
 
-  return res
-    .status(204)
-    .json({ status: 204, message: "User deleted succesfully" });
+  return res.status(204).send();
 };
