7f50ac15f59a8864d5053bea3ab878f8a2812c1e
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-23 15:36:06 +0200

    refactor(controllers): update data return in getUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,13 @@ export const getAllUsers = async (req, res) => {
 export const getUser = async (req, res) => {
   const user = await prisma.user.findUnique({
     where: { id: parseInt(req.user.id) },
-    include: { bookings: true },
+    include: {
+      bookings: {
+        include: {
+          car: { select: { carClass: true, model: true, brand: true } },
+        },
+      },
+    },
   });
 
   if (!user) {
