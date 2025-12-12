77b28f2fdd6ed5e8f83a3c485a600fa50f44474e
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-12 18:25:14 +0200

    chore(cors): add origin and credentials params

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/users", userRoute);
