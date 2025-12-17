8f3427307a8fe809e00885a10f6903c3db107532
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-17 14:41:01 +0200

    fixbug(middlewares): update status codes for unauthorised error

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -12,7 +12,9 @@ export const authenticateToken = (req, res, next) => {
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) {
-      const error = createHttpError(403, "Forbidden", { message: err.message });
+      const error = createHttpError(401, "Unauthorised", {
+        message: err.message,
+      });
       next(error);
     }
     req.user = user;
